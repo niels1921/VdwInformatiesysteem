70b0f288d94385d2bcda291c2bc91421d9b6f16b
Harm van den Bogert <harm.vandenbogert@outlook.com>
2018-01-16 11:00:36 +0100

    projectviewmodel

---
+++ Managementsysteem/Managementsysteem/Models/ViewModels/ProjectViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Managementsysteem.Models.ViewModels
+{
+    public class ProjectViewmodel
+    {
+        public Project Project { get; set; }
+        public IEnumerable<Gebeurtenis> Gebeurtenis { get; set; }
+        public IEnumerable<Taak> Taak { get; set; }
+    }
+}
